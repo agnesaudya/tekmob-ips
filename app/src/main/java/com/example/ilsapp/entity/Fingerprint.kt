2e421ce13b204e71be7d29172b54462f2483049a
haddad <haddad@haddad-macbook.local>
2023-12-06 15:39:15 +0700

    add rssi columns

--- app/src/main/java/com/example/ilsapp/entity/Fingerprint.kt
+++ app/src/main/java/com/example/ilsapp/entity/Fingerprint.kt
@@ -16,7 +16,16 @@ data class Fingerprint(
     @ColumnInfo(name = "bssid3_rssi") var bssid3_rssi: Int = Int.MAX_VALUE,
     @ColumnInfo(name = "bssid4_rssi") var bssid4_rssi: Int = Int.MAX_VALUE,
     @ColumnInfo(name = "bssid5_rssi") var bssid5_rssi: Int = Int.MAX_VALUE,
-    @ColumnInfo(name = "bssid6_rssi") var bssid6_rssi: Int = Int.MAX_VALUE
+    @ColumnInfo(name = "bssid6_rssi") var bssid6_rssi: Int = Int.MAX_VALUE,
+    @ColumnInfo(name = "bssid7_rssi") var bssid7_rssi: Int = Int.MAX_VALUE,
+    @ColumnInfo(name = "bssid8_rssi") var bssid8_rssi: Int = Int.MAX_VALUE,
+    @ColumnInfo(name = "bssid9_rssi") var bssid9_rssi: Int = Int.MAX_VALUE,
+    @ColumnInfo(name = "bssid10_rssi") var bssid10_rssi: Int = Int.MAX_VALUE,
+    @ColumnInfo(name = "bssid11_rssi") var bssid11_rssi: Int = Int.MAX_VALUE,
+    @ColumnInfo(name = "bssid12_rssi") var bssid12_rssi: Int = Int.MAX_VALUE,
+    @ColumnInfo(name = "bssid13_rssi") var bssid13_rssi: Int = Int.MAX_VALUE,
+    @ColumnInfo(name = "bssid14_rssi") var bssid14_rssi: Int = Int.MAX_VALUE,
+
 ) {
     // Primary constructor
     constructor() : this(0, "dummy label")
